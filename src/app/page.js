70f5a3ead6aa5d973aa6343a6300ed9db57cfe21
Aasha Shrestha <139418952+AashaShrestha2058@users.noreply.github.com>
2024-07-08 08:35:32 +0545

    Add teacher

--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,12 @@ import Teacher from "@/pages/admins/Teacher";
 import Footer from "@/components/Footer";
 import "tailwindcss/tailwind.css";
 import "@/styles/globals.css";
+import Login from "@/pages/Login";
 
 const Main = () => {
   return (
     <div>
-    <Menu/>
+    <Login/>
     </div>
   );
 };
