4e93f81a21cafbc407062f7d37bd664cea65c0a4
labinale45 <rabinale45@gmail.com>
2024-07-08 09:04:06 +0545

    Rabin

--- src/app/page.js
+++ src/app/page.js
@@ -3,35 +3,16 @@ import Home from "@/pages/Home";
 import Menu from "@/components/Menu";
 import Nav from "@/components/Nav";
 import Teacher from "@/pages/admins/Teacher";
-<<<<<<< HEAD
 import Try from "@/pages/admins/Try";
-=======
-import { Addstudent, Studenttable } from "@/pages/admins/Studenttable";
-
->>>>>>> 02a429a4dcf9866a09f87b49d2c88ab2ba64d698
 import Footer from "@/components/Footer";
 import "tailwindcss/tailwind.css";
 import "@/styles/globals.css";
-<<<<<<< HEAD
-import Login from "@/pages/Login";
-import Addteacher from "@/pages/admins/addTeacher";
-=======
-import { Examtable } from "@/pages/admins/Examtable";
 import { Teachertable } from "@/pages/admins/Teachertable";
->>>>>>> 02a429a4dcf9866a09f87b49d2c88ab2ba64d698
 
 const Main = () => {
   return (
     <div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <Try/>
-=======
-    <Addteacher/>
->>>>>>> c0c11451708739a761185d4216a1b7263c2130ed
-=======
-      <Teachertable />
->>>>>>> 02a429a4dcf9866a09f87b49d2c88ab2ba64d698
+      <Teachertable/>
     </div>
   );
 };
