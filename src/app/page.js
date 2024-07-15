6b71a0dadd8b2daa3ddcb3f880f6a92f5aea046f
labinale45 <rabinale45@gmail.com>
2024-07-15 06:54:07 +0545

    page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client"
 import "tailwindcss/tailwind.css";
 import "@/app/styles/globals.css";
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 const Main = () => {
